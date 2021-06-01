a048212d7742420b0bce1aa896653ea5d85f784c
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-06-01 22:54:05 +0300

    Tests Created

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+from .models import Profile , Post, Comment 
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.user = User(id=1, username='alex', password='1234')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+
+class PostTestClass (TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(post_title='test post',
+        post_image='png', post_description='bio',)
+
+   
+    
